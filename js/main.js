a75969f852f08dac8587036d865e18c0f1bb6b13
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-04-15 16:57:34 +0200

    fill out page and end page done
    the css and html for thees pages are done

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@ let spa = new Spa("login");
 window.pageChange = function(){
 spa.pageChange();
 }
+
+document.querySelector(".login-btn").addEventListener("click", navToFill)
+document.querySelector(".trunIn").addEventListener("click", navToEnd)
+
+function navToFill(){
+spa.navigateTo("fill")
+}
+function navToEnd(){
+spa.navigateTo("end")
+}
